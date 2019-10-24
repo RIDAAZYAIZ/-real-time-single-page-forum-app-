330c128efa74c83ad69acbd96044140a71ef8487
ridaaz <ridaazyaiz@gmail.com>
2019-10-25 00:29:19 +0100

    creation migration , model , controller ,factory

---
+++ database/migrations/2019_10_24_230424_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('title');
+            $table->String('slug');
+            $table->text('body');
+
+            $table->intger('category_id')->unsigned();
+            $table->intger('User_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
